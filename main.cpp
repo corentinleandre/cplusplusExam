30343b04f3763522e2b32a0c95050f600c5103a7
Corentin LEANDRE <corentinleandre@gmail.com>
2024-01-10 10:00:20 +0100

    added leo

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 int main() {
     Animal flash = Animal("Flash", "black", "Carnivore", "Mountains", "Awoooo", false, 9, 9.9, 1.8);
-    Mammal leo = Mammal("Leo", "black", "Carnivore", "Mountains", "Awoooo", false, 9, 9.9, 1.8);
+    Mammal leo = Mammal("Leo", "Golden", "Carnivore", "Grassland", "Roar", false, 5, 180.5, 3.5,"Tawny");
     flash.printInfo();
+    leo.printInfo();
     return 0;
 }
